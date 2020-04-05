594e216ed21d9cbab1871e0c09d986b27bf8ef02
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-05 19:30:33 +0530

    ADDED:Class For Invoice Summery

--- src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceSummery.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/InvoiceSummery.java
@@ -1,4 +1,32 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import java.util.Objects;
+
 public class InvoiceSummery {
+    //FIELDS
+    public int NumberOfRides;
+    public double totalTravelFareInRS;
+    public double averageFarePerRidesInRS;
+
+    //CONSTRUCTOR
+    public InvoiceSummery(int NumberOfRides, double totalTravelFareInRS) {
+        this.NumberOfRides = NumberOfRides;
+        this.totalTravelFareInRS = totalTravelFareInRS;
+        this.averageFarePerRidesInRS = totalTravelFareInRS / NumberOfRides;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummery summery = (InvoiceSummery) o;
+        return NumberOfRides == summery.NumberOfRides &&
+                Double.compare(summery.totalTravelFareInRS, totalTravelFareInRS) == 0 &&
+                Double.compare(summery.averageFarePerRidesInRS, averageFarePerRidesInRS) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(NumberOfRides, totalTravelFareInRS, averageFarePerRidesInRS);
+    }
 }
