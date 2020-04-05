8686ee174b917095978001ac9cd7248b02698d4e
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-05 14:59:09 +0530

    ADDED:TestCaseToCheckTravelFare

--- src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
@@ -1,4 +1,21 @@
 package com.bridgelabz.cabinvoicegeneratortest;
 
+import com.bridgelabz.cabinvoicegenerator.CabInvoiceGenerator;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CabInvoiceGeneratorTest {
+    @Test
+    public void givenDistanceAndTimeOfTravel_WhenMoreThanOneKMAndOneMinute_ShouldReturnTheTotalTravelFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double totalTravelFare = cabInvoiceGenerator.getTravelFare(10, 20);
+        Assert.assertEquals(120, totalTravelFare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTimeOfTravel_WhenLessThanOneKMAndOneMinute_ShouldReturnMinimumFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double totalTravelFare = cabInvoiceGenerator.getTravelFare(0, 0);
+        Assert.assertEquals(5, totalTravelFare, 0.0);
+    }
 }
