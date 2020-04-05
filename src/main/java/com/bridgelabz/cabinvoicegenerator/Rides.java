57a8b165028f2fd5fb34c73c6cef642f5983e2fe
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-05 16:42:43 +0530

    ADDED:Class For Multiple Rides

--- src/main/java/com/bridgelabz/cabinvoicegenerator/Rides.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/Rides.java
@@ -1,4 +1,13 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class Rides {
+    //FIELDS
+    public double travelDistanceInKM;
+    public double travelTimeInMinutes;
+
+    //PARAMETRISED CONSTRUCTOR
+    public Rides(double travelDistanceInKM, double travelTimeInMinutes) {
+        this.travelDistanceInKM = travelDistanceInKM;
+        this.travelTimeInMinutes = travelTimeInMinutes;
+    }
 }
