2ad12436098941a0407b0c121db259dea9f5afdb
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-05 16:44:22 +0530

    REFACTORED:Test Case To Check Values For Multiple Rides

--- src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegeneratortest/CabInvoiceGeneratorTest.java
@@ -1,21 +1,32 @@
 package com.bridgelabz.cabinvoicegeneratortest;
 
 import com.bridgelabz.cabinvoicegenerator.CabInvoiceGenerator;
+import com.bridgelabz.cabinvoicegenerator.Rides;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CabInvoiceGeneratorTest {
+    //GLOBAL OBJECT
+    CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+
     @Test
     public void givenDistanceAndTimeOfTravel_WhenMoreThanOneKMAndOneMinute_ShouldReturnTheTotalTravelFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-        double totalTravelFare = cabInvoiceGenerator.getTravelFare(10, 20);
-        Assert.assertEquals(120, totalTravelFare, 0.0);
+        Rides[] rides = {new Rides(5, 10)};
+        double totalTravelFare = cabInvoiceGenerator.getTravelFare(rides);
+        Assert.assertEquals(60, totalTravelFare, 0.0);
     }
 
     @Test
     public void givenDistanceAndTimeOfTravel_WhenLessThanOneKMAndOneMinute_ShouldReturnMinimumFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-        double totalTravelFare = cabInvoiceGenerator.getTravelFare(0, 0);
+        Rides[] rides = {new Rides(0.5, 0)};
+        double totalTravelFare = cabInvoiceGenerator.getTravelFare(rides);
         Assert.assertEquals(5, totalTravelFare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTimeOfTravelForMultipleRides_ShouldReturnTheTotalTravelFare() {
+        Rides[] rides = {new Rides(0.5, 0), new Rides(5, 10)};
+        double totalTravelFare = cabInvoiceGenerator.getTravelFare(rides);
+        Assert.assertEquals(65, totalTravelFare, 0.0);
     }
 }
