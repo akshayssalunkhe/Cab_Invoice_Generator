8cd2ebe7bf2a636930746abcb4891609d14bfce6
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-06 13:17:22 +0530

    ADDED:Class To Add Ride Repository

--- src/main/java/com/bridgelabz/cabinvoicegenerator/RideRepository.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/RideRepository.java
@@ -1,4 +1,20 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RideRepository {
+    Map<String, Rides[]> rideList;
+
+    public RideRepository() {
+        this.rideList = new HashMap<>();
+    }
+
+    public void addRide(String userId, Rides[] rides) {
+        rideList.put(userId, rides);
+    }
+
+    public Rides[] getRideList(String userId) {
+        return rideList.get(userId);
+    }
 }
