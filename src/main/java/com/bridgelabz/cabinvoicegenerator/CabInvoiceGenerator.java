4cc2c65267e7033de42bfcf060272f7c9dfd6100
akshayssalunkhe <akshayssalunkhe100@gmail.com>
2020-04-06 13:16:02 +0530

    ADDED:Method To Get Invoice Summery As Per User ID

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,9 +1,17 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
+
+    //CONSTANTS
     public static final double COST_PER_KILOMETER_IN_RS = 10;
     public static final double COST_PER_MINUTE_IN_RS = 1;
     private static final double MINIMUM_TRAVEL_FARE_IN_RS = 5;
+
+    RideRepository rideRepository;
+
+    public CabInvoiceGenerator() {
+        this.rideRepository = new RideRepository();
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome To Cab Invoice Generator");
@@ -18,9 +26,21 @@ public class CabInvoiceGenerator {
         return Math.max(totalTravelFareInRS, MINIMUM_TRAVEL_FARE_IN_RS);
     }
 
-    //METHOD TO GET INVOICE SUMMERY
+    //METHOD TO GET INVOICE SUMMERY BY PASSING RIDES
     public InvoiceSummery getInvoiceSummery(Rides[] rides) {
         double totalTravelFareInRS = getTravelFare(rides);
         return new InvoiceSummery(rides.length, totalTravelFareInRS);
+    }
+
+    //METHOD TO GET INVOICE SUMMERY BY PASSING USER ID
+    public InvoiceSummery getInvoiceSummery(String userId) {
+        Rides[] rideList = rideRepository.getRideList(userId);
+        double totalTravelFareInRS = getTravelFare(rideList);
+        return new InvoiceSummery(rideList.length, totalTravelFareInRS);
+    }
+
+    //METHOD TO ADD RIDE LIST
+    public void addRides(String userId, Rides[] rides) {
+        rideRepository.addRide(userId, rides);
     }
 }
